f1f4ad4ac0ec045d8f9e71724be15946f1d49203
Kye <kye@apacmediasolutions.com>
2023-10-07 23:23:19 -0400

    kosmosg

---
+++ example.py
@@ -0,0 +1,10 @@
+import torch
+from kosmog.main import KosmosG
+
+# usage
+img = torch.randn(1, 3, 256, 256)
+text = torch.randint(0, 20000, (1, 1024))
+
+model = KosmosG()
+output = model(img, text)
+print(output)
